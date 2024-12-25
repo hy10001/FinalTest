2f2b5bdb164ffdccd35d2e8a442e698372a3904d
XRC <2257956574@qq.com>
2024-12-25 16:20:52 +0800

    XRC

--- src/main/java/cn/lanqiao/finalTest/controller/inventoryController.java
+++ src/main/java/cn/lanqiao/finalTest/controller/inventoryController.java
@@ -1,4 +1,9 @@
 package cn.lanqiao.finalTest.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/inventory")
 public class inventoryController {
 }
