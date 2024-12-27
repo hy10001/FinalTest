2a2c457f72bab422e4441d0327c1fd452f2088e1
XRC <2257956574@qq.com>
2024-12-27 17:40:40 +0800

    XRC

--- src/main/java/cn/lanqiao/finalTest/controller/inventoryController.java
+++ src/main/java/cn/lanqiao/finalTest/controller/inventoryController.java
@@ -1,6 +1,7 @@
 package cn.lanqiao.finalTest.controller;
 
 import cn.lanqiao.finalTest.model.dto.inventory.InventoryAdd;
+import cn.lanqiao.finalTest.model.dto.inventory.InventoryEdit;
 import cn.lanqiao.finalTest.service.InventoryService;
 import cn.lanqiao.finalTest.utils.ResponseUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,21 @@ public class inventoryController {
         try {
             int result = inventoryService.add_stock(inventoryAdd);
             if (result == 1) {
+                return new ResponseUtils(200, "入库登记成功");
+            } else {
+                return new ResponseUtils(300, "入库登记失败");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    @RequestMapping("/update_stock")
+    public ResponseUtils update_stock(@RequestBody InventoryEdit inventoryEdit){
+        try {
+            int result = inventoryService.update_stock(inventoryEdit);
+            if (result == 1) {
                 return new ResponseUtils(200, "入库成功");
             } else {
                 return new ResponseUtils(300, "入库失败");
@@ -28,5 +44,18 @@ public class inventoryController {
             throw new RuntimeException(e);
         }
     }
-
+    @RequestMapping("/delete_stock")
+    public ResponseUtils delete_stock(@RequestBody InventoryAdd inventoryAdd){
+        try {
+            int result = inventoryService.delete_stock(inventoryAdd.getItemName());
+            if (result == 1) {
+                return new ResponseUtils(200, "出库成功");
+            } else {
+                return new ResponseUtils(300, "出库失败");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/main/java/cn/lanqiao/finalTest/service/Impl/InventoryServiceImpl.java
+++ src/main/java/cn/lanqiao/finalTest/service/Impl/InventoryServiceImpl.java
@@ -2,6 +2,7 @@ package cn.lanqiao.finalTest.service.Impl;
 
 import cn.lanqiao.finalTest.mapper.InventoryMapper;
 import cn.lanqiao.finalTest.model.dto.inventory.InventoryAdd;
+import cn.lanqiao.finalTest.model.dto.inventory.InventoryEdit;
 import cn.lanqiao.finalTest.service.InventoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,26 @@ public class InventoryServiceImpl implements InventoryService {
     private InventoryMapper inventoryMapper;
     public int add_stock(InventoryAdd inventoryAdd) {
         int result = inventoryMapper.add_stock(inventoryAdd);
+        if (result > 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    @Override
+    public int update_stock(InventoryEdit inventoryEdit) {
+        int result = inventoryMapper.update_stock(inventoryEdit);
+        if (result > 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    @Override
+    public int delete_stock(String itemName) {
+        int result = inventoryMapper.delete_stock(itemName);
         if (result > 0) {
             return 1;
         } else {
