5d4d33b29d8c36704f62c990fc6b8b898c13bc17
李远发 <3206159221@qq.com>
2024-12-30 13:31:07 +0800

    lyf 20201230

--- src/main/java/cn/lanqiao/finalTest/controller/AdminController.java
+++ src/main/java/cn/lanqiao/finalTest/controller/AdminController.java
@@ -1,6 +1,7 @@
 package cn.lanqiao.finalTest.controller;
 
-import cn.lanqiao.finalTest.utils.ResponseUtils;
+
+import cn.lanqiao.finalTest.Utils.ResponseUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
