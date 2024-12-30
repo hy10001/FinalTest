d2b043d55c5e706affc13f3b80eee1010ab8b47d
hy <2012602947@qq.com>
2024-12-30 13:08:42 +0800

    何益

---
+++ src/main/java/cn/lanqiao/finalTest/dao/Admin.java
@@ -0,0 +1,16 @@
+package cn.lanqiao.finalTest.dao;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Admin {
+
+  private String username;
+  private String password;
+  private String is_delete;
+
+}
